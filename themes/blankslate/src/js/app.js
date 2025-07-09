37636a8d0dc3dbd84260cd212fda118d40088735
Ala Baganne <alabaganne9@gmail.com>
2025-07-09 13:56:51 +0100

    use astra as base theme

--- themes/blankslate/src/js/app.js
+++
@@ -1,9 +0,0 @@
-// Main JavaScript file for the theme
-// You can add your custom JavaScript here
-
-console.log('Theme JavaScript loaded');
-
-// Example: Mobile menu toggle
-document.addEventListener('DOMContentLoaded', function() {
-    // Add your JavaScript functionality here
-});
